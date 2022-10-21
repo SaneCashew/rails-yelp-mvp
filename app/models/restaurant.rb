d2841ee2ffb2753bb64f5382b93f25111ddb1f28
Jonathan Troupe <sanetroupe@gmail.com>
2022-10-20 20:39:27 -0400

    Added review functionality

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   CATEGORIES = %w[french japanese italian chinese belgian].freeze
-
   validates :name, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: CATEGORIES }
